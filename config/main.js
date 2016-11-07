78e6985fc2999332a8d095276ed8e1de560f1316
Juan Pablo Franco <jfranco@ooyala.com>
2016-11-07 10:01:50 -0800

    Workaround for issues caused by tmpdir using symlinks on Mac

--- config/main.js
+++ config/main.js
@@ -1,11 +1,10 @@
-const os = require('os');
-const fs = require('fs-extra');
 const path = require('path');
+const utils =  require('../lib/utils');
 
 module.exports = {
   RESOURCE_ROOT: 'http://player.ooyala.com/static/v4',
   PROJECT_PATH: path.join(__dirname, '..'),
-  BUILD_PATH: fs.realpathSync(path.join(os.tmpdir(), 'oppm')),
+  BUILD_PATH: utils.ensureTmpDir('oppm'), // On MacOS, the path returned by os.tmpdir() is a symlink
   VERSION_CHECK_ASSET: 'core.min.js',
   VERSION_FILE: 'version.txt',
   BUNDLE_FILE: 'player_bundle.js'
